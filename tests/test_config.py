96b43d76310538701d116b9b93f7d3701bb84dbf
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-21 14:15:30 +0900

    fix(config): instantiate Config after monkeypatch

--- tests/test_config.py
+++ tests/test_config.py
@@ -18,17 +18,17 @@ def test_config_returns_environment_variables(monkeypatch):
     monkeypatch.setenv('FERNET_KEY', 'fernet-key')
     monkeypatch.setenv('SECRET_KEY', 'secret-key')
 
-config = Config()
+    config = Config()
 
-assert config.AWS_COGNITO_AUTHORITY == 'https://authority'
-assert config.AWS_COGNITO_CLIENT_SECRET == 'client-secret'
-assert config.AWS_COGNITO_DOMAIN == 'https://domain'
-assert config.AWS_COGNITO_LOGOUT_URI == 'http://logout-uri'
-assert config.AWS_COGNITO_METADATA_URL == 'https://metadata-url'
-assert config.AWS_COGNITO_REDIRECT_URI == 'https://redirect-uri'
-assert config.AWS_COGNITO_SCOPE == 'scope'
-assert config.AWS_COGNITO_USER_POOL_CLIENT_ID == 'user-pool-client-id'
-assert config.AWS_COGNITO_USER_POOL_ID == 'user-pool-id'
-assert config.AWS_REGION == 'region'
-assert config.FERNET_KEY == 'fernet-key'
-assert config.SECRET_KEY == 'secret-key'
+    assert config.AWS_COGNITO_AUTHORITY == 'https://authority'
+    assert config.AWS_COGNITO_CLIENT_SECRET == 'client-secret'
+    assert config.AWS_COGNITO_DOMAIN == 'https://domain'
+    assert config.AWS_COGNITO_LOGOUT_URI == 'http://logout-uri'
+    assert config.AWS_COGNITO_METADATA_URL == 'https://metadata-url'
+    assert config.AWS_COGNITO_REDIRECT_URI == 'https://redirect-uri'
+    assert config.AWS_COGNITO_SCOPE == 'scope'
+    assert config.AWS_COGNITO_USER_POOL_CLIENT_ID == 'user-pool-client-id'
+    assert config.AWS_COGNITO_USER_POOL_ID == 'user-pool-id'
+    assert config.AWS_REGION == 'region'
+    assert config.FERNET_KEY == 'fernet-key'
+    assert config.SECRET_KEY == 'secret-key'
