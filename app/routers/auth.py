1a64352cd319a81e312aeca41a6a580f52dc1a98
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-22 20:56:20 +0900

    chore(login): add return type annotation

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
 from app.utils.auth_helpers import redirect_to_cognito_login
 
 router = APIRouter()
 
 @router.get('/login')
-async def login(request: Request):
+async def login(request: Request) -> RedirectResponse:
     return await redirect_to_cognito_login(request)
 
 @router.get('/callback')
