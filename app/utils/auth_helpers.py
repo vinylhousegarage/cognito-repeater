a0a7f412d85be7283716ccb94b3424e2edb3beb2
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-23 15:36:47 +0900

    refactor(helper): use cache_cognito_metadata in redirect_to_cognito_login; move function to bottom

--- app/utils/auth_helpers.py
+++ app/utils/auth_helpers.py
@@ -1,23 +1,7 @@
 import httpx
 from fastapi import Request
 from fastapi.responses import RedirectResponse
-
-async def redirect_to_cognito_login(request: Request) -> RedirectResponse:
-    config = request.app.state.config
-    async with httpx.AsyncClient() as client:
-        metadata = await fetch_cognito_metadata(client, config.AWS_COGNITO_METADATA_URL)
-
-    login_url = metadata['authorization_endpoint']
-
-    params = {
-        'client_id': config.AWS_COGNITO_USER_POOL_CLIENT_ID,
-        'response_type': 'code',
-        'scope': config.AWS_COGNITO_SCOPE,
-        'redirect_uri': config.AWS_COGNITO_REDIRECT_URI,
-    }
-    from urllib.parse import urlencode
-    full_url = f'{login_url}?{urlencode(params)}'
-    return RedirectResponse(url=full_url)
+from urllib.parse import urlencode
 
 async def fetch_cognito_metadata(client: httpx.AsyncClient, metadata_url: str) -> dict:
     response = await client.get(metadata_url)
@@ -31,3 +15,18 @@ async def cache_cognito_metadata(app) -> dict:
               client, app.state.config.AWS_COGNITO_METADATA_URL
             )
     return app.state.metadata
+
+
+async def redirect_to_cognito_login(request: Request) -> RedirectResponse:
+    app = request.app
+    config = request.app.state.config
+    metadata = await cache_cognito_metadata(app)
+    login_url = metadata['authorization_endpoint']
+    params = {
+        'client_id': config.AWS_COGNITO_USER_POOL_CLIENT_ID,
+        'response_type': 'code',
+        'scope': config.AWS_COGNITO_SCOPE,
+        'redirect_uri': config.AWS_COGNITO_REDIRECT_URI,
+    }
+    full_url = f'{login_url}?{urlencode(params)}'
+    return RedirectResponse(url=full_url)
