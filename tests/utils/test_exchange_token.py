8e8f667eea5b557124c15bbf632a3aefed6d9b9c
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-25 06:32:06 +0900

    fix(test): mock cache_cognito_metadata in test_create_token_request_payload

--- tests/utils/test_exchange_token.py
+++ tests/utils/test_exchange_token.py
@@ -1,9 +1,15 @@
+import app.utils.auth_helpers as auth_helpers
 from app.utils.token_helpers import create_token_request_payload
 
-async def test_create_token_request_payload(app):
-    app.state.metadata = {'token_endpoint': 'https://example.com/oauth2/token'}
-    metadata = app.state.metadata
-    url = metadata['token_endpoint']
+async def test_create_token_request_payload(monkeypatch, app):
+    dummy_metadata = {'token_endpoint': 'https://example.com/oauth2/token'}
+
+    async def fake_cache_cognito_metadata(_):
+          return dummy_metadata
+
+    monkeypatch.setattr(auth_helpers, 'cache_cognito_metadata', fake_cache_cognito_metadata)
+
+    url = dummy_metadata['token_endpoint']
 
     code = 'abc1234'
     config = app.state.config
