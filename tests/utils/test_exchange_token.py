9245cf2b1ac008ce4d0c3aac389ec0fa5b6340af
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-24 18:49:05 +0900

    fix(test): add dummy data to app.state.metadata

--- tests/utils/test_exchange_token.py
+++ tests/utils/test_exchange_token.py
@@ -2,10 +2,11 @@ from app.utils.token_helpers import create_token_request_url
 from urllib.parse import urlencode
 
 async def test_create_token_request_url(app):
+    app.state.metadata = {'token_endpoint': 'https://example.com/oauth2/token'}
     metadata = app.state.metadata
-    config = app.state.config
-
     endpoint = metadata['token_endpoint']
+
+    config = app.state.config
     params = {
         'grant_type': 'authorization_code',
         'client_id': config.AWS_COGNITO_USER_POOL_CLIENT_ID,
