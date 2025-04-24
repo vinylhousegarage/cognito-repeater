812d13b6fcba1f6fe3096898ab23b95a1ae28bf3
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-24 19:56:20 +0900

    test(helper): replace url creation test with payload creation test

--- tests/utils/test_exchange_token.py
+++ tests/utils/test_exchange_token.py
@@ -1,19 +1,23 @@
-from app.utils.token_helpers import create_token_request_url
-from urllib.parse import urlencode
+from app.utils.token_helpers import create_token_request_payload
 
-async def test_create_token_request_url(app):
+async def test_create_token_request_payload(app):
     app.state.metadata = {'token_endpoint': 'https://example.com/oauth2/token'}
     metadata = app.state.metadata
-    endpoint = metadata['token_endpoint']
+    url = metadata['token_endpoint']
 
+    code = 'abc1234'
     config = app.state.config
-    params = {
+    data = {
+        'code': code,
+        'redirect_url': config.AWS_COGNITO_REDIRECT_URI,
         'grant_type': 'authorization_code',
         'client_id': config.AWS_COGNITO_USER_POOL_CLIENT_ID,
         'client_secret': config.AWS_COGNITO_CLIENT_SECRET,
     }
 
-    expected = f'{endpoint}?{urlencode(params)}'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-    result = await create_token_request_url(app)
-    assert result == expected
+    payload = {'url': url, 'data': data, 'headers': headers}
+
+    result = await create_token_request_payload(app, code)
+    assert result == payload
