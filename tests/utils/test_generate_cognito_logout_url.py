d1401209d389c2c7ed0efebdda4751d7e33fbbd2
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-25 17:31:56 +0900

    test(helper): add test_generate_cognito_logout_url

---
+++ tests/utils/test_generate_cognito_logout_url.py
@@ -0,0 +1,21 @@
+import app.utils.auth_helpers as auth_helpers
+from urllib.parse import urlencode
+
+async def test_generate_cognito_logout_url(app, monkeypatch):
+    dummy_metadata = {'logout_endpoint': 'https://dummy.com/logout/'}
+    async def fake_cache_cognito_metadata(app):
+        return dummy_metadata
+
+    monkeypatch.setattr(auth_helpers, 'cache_cognito_metadata', fake_cache_cognito_metadata)
+
+    endpoint = dummy_metadata['logout_endpoint']
+    params = {
+        'client_id': 'dummy-id',
+        'logout_uri': 'dummy-uri',
+    }
+
+    expected_url = f'{endpoint}?{urlencode(params)}'
+
+    logout_url = await auth_helpers.generate_cognito_logout_url(app)
+
+    assert logout_url == expected_url
