1017f9d8d20ddcc34b5f4c1b66deb16f06be9a27
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-25 10:31:01 +0900

    test(helper): add test_exchange_token

--- tests/utils/test_exchange_token.py
+++ tests/utils/test_exchange_token.py
@@ -26,3 +26,33 @@ async def test_create_token_request_payload(monkeypatch, app):
 
     result = await token_helpers.create_token_request_payload(app, code)
     assert result == payload
+
+async def test_exchange_token(httpx_mock, monkeypatch, app):
+    dummy_code = 'abc1234'
+
+    dummy_payload = {
+        'url': 'https://example.com/token',
+        'data': {'code': 'abc1234'},
+        'headers': {'Content-Type': 'application/x-www-form-urlencoded'}
+    }
+
+    dummy_response_data = {
+        'access_token': 'dummy_access',
+        'id_token': 'dummy_id',
+        'refresh_token': 'dummy_refresh',
+        'token_type': 'Bearer',
+        'expires_in': 3600,
+    }
+
+    async def fake_create_token_request_payload(app, code):
+        return dummy_payload
+
+    monkeypatch.setattr(token_helpers, 'create_token_request_payload', fake_create_token_request_payload)
+
+    httpx_mock.add_response(
+        url=dummy_payload['url'],
+        json=dummy_response_data,
+    )
+
+    result = await token_helpers.exchange_token(app, dummy_code)
+    assert result == dummy_response_data
