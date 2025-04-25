8c818ed2b88ff8393e5a64b2b6f75702d1e56dde
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-25 18:30:01 +0900

    fix(test): mock fastapi Request in test

--- tests/utils/test_generate_cognito_logout_url.py
+++ tests/utils/test_generate_cognito_logout_url.py
@@ -1,22 +1,29 @@
 import app.utils.auth_helpers as auth_helpers
 from fastapi import Request
+from types import SimpleNamespace
 from urllib.parse import urlencode
 
-async def test_generate_cognito_logout_url(request: Request, monkeypatch):
-    dummy_metadata = {'logout_endpoint': 'https://dummy.com/logout/'}
+async def test_generate_cognito_logout_url(monkeypatch):
+    dummy_metadata = {'end_session_endpoint': 'https://dummy.com/logout/'}
+
     async def fake_cache_cognito_metadata(app):
         return dummy_metadata
 
     monkeypatch.setattr(auth_helpers, 'cache_cognito_metadata', fake_cache_cognito_metadata)
 
-    endpoint = dummy_metadata['logout_endpoint']
+    endpoint = dummy_metadata['end_session_endpoint']
     params = {
         'client_id': 'dummy-id',
         'logout_uri': 'dummy-uri',
     }
-
     expected_url = f'{endpoint}?{urlencode(params)}'
 
-    logout_url = await auth_helpers.generate_cognito_logout_url(request)
+    dummy_config = SimpleNamespace(
+        AWS_COGNITO_USER_POOL_CLIENT_ID='dummy-id',
+        AWS_COGNITO_LOGOUT_URI='dummy-uri',
+    )
+    dummy_app = SimpleNamespace(state=SimpleNamespace(config=dummy_config))
+    dummy_request = SimpleNamespace(app=dummy_app)
+    logout_url = await auth_helpers.generate_cognito_logout_url(dummy_request)
 
     assert logout_url == expected_url
