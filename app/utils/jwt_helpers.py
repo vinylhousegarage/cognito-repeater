2938551217f28e6a953f966aac6278dafa28c61f
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-27 05:09:54 +0900

    fix(helper): remove dependency injection for async client

--- app/utils/jwt_helpers.py
+++ app/utils/jwt_helpers.py
@@ -2,15 +2,11 @@ from fastapi import Request
 from httpx import AsyncClient
 from app.utils.auth_helpers import cache_cognito_metadata
 
-async def fetch_cognito_jwks(request: Request, client: AsyncClient = None) -> dict:
+async def fetch_cognito_jwks(request: Request) -> dict:
     metadata = await cache_cognito_metadata(request)
     uri = metadata['jwks_uri']
 
-    if client is None:
-        async with AsyncClient() as new_client:
-            response = await new_client.get(url=uri)
-            response.raise_for_status()
-    else:
+    async with AsyncClient() as client:
         response = await client.get(url=uri)
         response.raise_for_status()
 
