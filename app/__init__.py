f6e46701d91f9dcbcf4a70b071677cda5b9226de
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-26 13:53:15 +0900

    update(init): add HTTPBearer instance to app.state

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPBearer
 from app.config import Config
 from app.routers import auth, health
 
@@ -7,6 +7,9 @@ def create_app() -> FastAPI:
 
     config = Config()
     app.state.config = config
+
+    bearer_scheme = HTTPBearer()
+    app.state.bearer_scheme = bearer_scheme
 
     app.include_router(auth.router)
     app.include_router(health.router)
