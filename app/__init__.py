f6457b38033a5e83820fdd251500dd5daa588480
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-26 14:17:32 +0900

    refactor(init): remove HTTPBearer instance from app.state

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPBearer
+from fastapi import FastAPI
 from app.config import Config
 from app.routers import auth, health
 
@@ -7,9 +7,6 @@ def create_app() -> FastAPI:
 
     config = Config()
     app.state.config = config
-
-    bearer_scheme = HTTPBearer()
-    app.state.bearer_scheme = bearer_scheme
 
     app.include_router(auth.router)
     app.include_router(health.router)
