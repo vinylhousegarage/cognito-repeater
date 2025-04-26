4a4844f342252987c8eee3e25bd762501f1a06c9
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-26 14:43:21 +0900

    refactor(test): replace helper creation with get request in test

--- tests/routers/auth/test_me.py
+++ tests/routers/auth/test_me.py
@@ -1,13 +1,10 @@
-from app.utils import auth_helpers
+async def test_get_access_token(app_client):
+    dummy_access_token = 'dummy-access-token'
 
-async def test_get_access_token(monkeypatch):
-    dummy_access_token = {'key': 'value'}
+    response = await app_client.get(
+        '/me',
+        headers={'Authorization': f'Bearer {dummy_access_token}'}
+    )
 
-    async def fake_get_access_token(*args, **kwargs):
-        return dummy_access_token
-
-    monkeypatch.setattr(auth_helpers, 'get_access_token', fake_get_access_token)
-
-    access_token = await auth_helpers.get_access_token()
-
-    assert access_token == dummy_access_token
+    assert response.status_code == 200
+    assert 'user' in response.json()
