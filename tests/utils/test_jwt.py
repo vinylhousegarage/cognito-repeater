b14c38e66025d96e1cd2e01a0f7c97888e2a0bd9
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-27 06:50:23 +0900

    fix(test): replace mock target with httpx_mock

--- tests/utils/test_jwt.py
+++ tests/utils/test_jwt.py
@@ -1,28 +1,24 @@
+from pytest_httpx import HTTPXMock
 from types import SimpleNamespace
 from app.utils import jwt_helpers
 
-async def test_fetch_cognito_jwks(async_client, monkeypatch):
-    dummy_response = {
-        'authorization_endpoint': 'https://example.com/oauth2/authorize',
-        'token_endpoint': 'https://example.com/oauth2/token',
-        'jwks_uri': 'https://example.com/.well-known/jwks.json'
+async def test_fetch_cognito_jwks(httpx_mock: HTTPXMock):
+    dummy_metadata = {'jwks_uri': 'https://example.com/jwks'}
+
+    dummy_jwks = {
+        'keys': [
+            {'kty': 'RSA', 'kid': 'dummy-kid', 'n': 'dummy-n', 'e': 'dummy-e'}
+        ]
     }
 
-    async def fake_get(url, *args, **kwargs):
-        class DummyResponse:
-            def raise_for_status(self): pass
-            def json(self): return dummy_response
-        return DummyResponse()
-
-    monkeypatch.setattr(async_client, 'get', fake_get)
+    httpx_mock.add_response(url=dummy_metadata['jwks_uri'], json=dummy_jwks)
 
     request = SimpleNamespace()
     request.app =SimpleNamespace()
     request.app.state = SimpleNamespace()
-    request.app.state.config = SimpleNamespace()
-    request.app.state.config.AWS_COGNITO_METADATA_URL = 'https://example.com/metadata'
+    request.app.state.metadata = SimpleNamespace()
+    request.app.state.metadata = dummy_metadata
 
-    metadata = await jwt_helpers.fetch_cognito_jwks(request, client=async_client)
-    uri = metadata['jwks_uri']
+    result = await jwt_helpers.fetch_cognito_jwks(request)
 
-    assert uri == dummy_response['jwks_uri']
+    assert result == dummy_jwks
