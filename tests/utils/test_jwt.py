0a270905693b1a1796c0396298037e26baf34255
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-27 08:31:55 +0900

    feat(test): add test_decode_access_token_for_kid

--- tests/utils/test_jwt.py
+++ tests/utils/test_jwt.py
@@ -1,3 +1,4 @@
+import jwt
 from pytest_httpx import HTTPXMock
 from types import SimpleNamespace
 from app.utils import jwt_helpers
@@ -22,3 +23,17 @@ async def test_fetch_cognito_jwks(httpx_mock: HTTPXMock):
     result = await jwt_helpers.fetch_cognito_jwks(request)
 
     assert result == dummy_jwks
+
+async def test_decode_access_token_for_kid():
+    dummy_kid = 'dummy-key-id'
+
+    dummy_token = jwt.encode(
+        payload={'sub': 'user-id'},
+        key='secret',
+        algorithm='HS256',
+        headers={'kid': dummy_kid},
+    )
+
+    result = await jwt_helpers.decode_access_token_for_kid(dummy_token)
+
+    assert result == dummy_kid
