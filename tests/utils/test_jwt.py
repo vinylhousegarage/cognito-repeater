ca865dfc088946ca8eeb042c09f40408ab8f117e
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-29 10:10:47 +0900

    feat(test): add test_cache_public_key_by_kid

--- tests/utils/test_jwt.py
+++ tests/utils/test_jwt.py
@@ -93,3 +93,7 @@ def test_generate_public_key(dummy_e_int, dummy_n_int, dummy_public_key):
 def test_cache_public_key(dummy_request, dummy_public_key):
     jwt_helpers.cache_public_key(dummy_request, dummy_public_key)
     assert dummy_request.app.state.public_key == dummy_public_key
+
+def test_cache_public_key_by_kid(dummy_request, dummy_kid, dummy_public_key):
+    jwt_helpers.cache_public_key_by_kid(dummy_request, dummy_kid, dummy_public_key)
+    assert dummy_request.app.state.public_keys[dummy_kid] == dummy_public_key
