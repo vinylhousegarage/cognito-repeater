925d7dbae5d4fd481e2de727d44829463bfc60bc
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-01 15:40:14 +0900

    fix(test): remove verify_access_token_claim_errors

--- tests/utils/test_jwt.py
+++ tests/utils/test_jwt.py
@@ -134,19 +134,3 @@ def test_verify_access_token_exceptions(mocked_exception, expected_detail, monke
 
     assert exc_info.value.status_code == 401
     assert expected_detail in exc_info.value.detail['error']
-
-@pytest.mark.parametrize('broken_payload, expected_error', [
-    ({'iss': 'wrong-audience'}, 'Invalid iss claims'),
-    ({'aud': 'wrong-audience'}, 'Invalid aud claims'),
-])
-def test_verify_access_token_claim_errors(broken_payload, expected_error, dummy_access_token_factory, dummy_leeway, dummy_request_for_verify, dummy_payload, dummy_public_key_for_verify):
-    payload = dummy_payload.copy()
-    payload.update(broken_payload)
-
-    dummy_access_token = dummy_access_token_factory(payload)
-
-    with pytest.raises(HTTPException) as exc:
-        jwt_helpers.verify_access_token(dummy_request_for_verify, dummy_access_token, dummy_public_key_for_verify, dummy_leeway)
-
-    assert exc.value.status_code == 401
-    assert exc.value.detail['error'] == expected_error
