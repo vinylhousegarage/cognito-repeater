b7de176fa8850053a182565995f6ff6c6300fcce
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-02 14:58:12 +0900

    refactor(test): add None case to invalid format test

--- tests/utils/test_jwt.py
+++ tests/utils/test_jwt.py
@@ -205,6 +205,7 @@ def test_verify_access_token_fails_without_public_key(dummy_request_for_verify, 
 
 @pytest.mark.parametrize('invalid_token', [
     '',  # empty string
+    None,  # None value
 ])
 def test_verify_access_token_invalid_format(invalid_token, dummy_request_for_verify, dummy_public_key_for_verify):
     with pytest.raises(HTTPException) as exc:
