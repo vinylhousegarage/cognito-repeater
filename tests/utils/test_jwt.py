7cd3818838063972ff1a3131a6bb8da24094405f
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-02 15:35:31 +0900

    fix(test): fix status_code assertion from 400 to 401; replace exact match with partial match

--- tests/utils/test_jwt.py
+++ tests/utils/test_jwt.py
@@ -223,5 +223,5 @@ def test_verify_access_token_missing_token(invalid_token, dummy_request_for_veri
 def test_verify_access_token_invalid_format(invalid_token, dummy_request_for_verify, dummy_public_key_for_verify):
     with pytest.raises(HTTPException) as exc:
         jwt_helpers.verify_access_token(dummy_request_for_verify, invalid_token, dummy_public_key_for_verify)
-    assert exc.value.status_code == 400
-    assert exc.value.detail['error'] == 'Malformed token'
+    assert exc.value.status_code == 401
+    assert 'Invalid header string' in exc.value.detail['error']
