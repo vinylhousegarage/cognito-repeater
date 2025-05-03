17b1f0e91bfe04949accb5d7a128d8ae72d140cf
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-03 17:26:02 +0900

    refactor(fixture): move shared fixtures to common_fixture.py and import explicitly in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,44 @@
-import pytest
-from fastapi.testclient import TestClient
-from httpx import AsyncClient, ASGITransport
-from app import create_app
+from tests.fixtures.common_fixture import (
+    app,
+    app_client,
+    async_client,
+    dummy_code,
+    test_client,
+)
 
-@pytest.fixture
-def app():
-    return create_app()
+from tests.fixtures.jwt_unit_fixtures import (
+    dummy_kid,
+    dummy_jwks_metadata,
+    dummy_app_attr_state,
+    dummy_request,
+    dummy_jwks_request,
+    fetch_cognito_jwks_httpx_mock,
+    dummy_e_int,
+    dummy_p_int,
+    dummy_q_int,
+    dummy_n_int,
+    dummy_public_key,
+    dummy_second_q_int,
+    dummy_second_n_int,
+    dummy_second_public_key,
+    dummy_private_key_for_verify,
+    dummy_public_key_for_verify,
+    dummy_private_key_for_verify_to_pem,
+    dummy_second_private_key_for_verify,
+    dummy_second_public_key_for_verify,
+    dummy_leeway,
+    dummy_request_for_verify,
+    dummy_claims,
+    dummy_access_token_factory,
+    dummy_access_token,
+    dummy_payload_factory,
+)
 
-@pytest.fixture
-async def app_client(app):
-    transport = ASGITransport(app=app)
-    async with AsyncClient(transport=transport, base_url='http://test') as app_client:
-        yield app_client
-
-@pytest.fixture
-async def async_client():
-    async with AsyncClient() as async_client:
-        yield async_client
-
-@pytest.fixture
-def dummy_code():
-    return 'abc1234'
-
-@pytest.fixture
-def test_client(app):
-    return TestClient(app)
+from tests.fixtures.me_fixtures import (
+    test_kid,
+    test_private_key,
+    test_private_key_pem,
+    test_claims,
+    test_access_token,
+    cache_cognito_metadata_httpx_mock,
+)
---
+++ tests/fixtures/common_fixture.py
@@ -0,0 +1,27 @@
+import pytest
+from fastapi.testclient import TestClient
+from httpx import AsyncClient, ASGITransport
+from app import create_app
+
+@pytest.fixture
+def app():
+    return create_app()
+
+@pytest.fixture
+async def app_client(app):
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url='http://test') as app_client:
+        yield app_client
+
+@pytest.fixture
+async def async_client():
+    async with AsyncClient() as async_client:
+        yield async_client
+
+@pytest.fixture
+def dummy_code():
+    return 'abc1234'
+
+@pytest.fixture
+def test_client(app):
+    return TestClient(app)
