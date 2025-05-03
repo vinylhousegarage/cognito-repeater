df054e7b835943dfa1eb2017e81592987c7145c4
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-03 10:41:50 +0900

    fix(test): fix typo (cacsh -> cache)

--- tests/routers/auth/test_me.py
+++ tests/routers/auth/test_me.py
@@ -1,6 +1,6 @@
 from httpx import AsyncClient
 
-async def test_me_returns_sub(app_client: AsyncClient, dummy_access_token: str, cacsh_cognito_metadata_httpx_mock):
+async def test_me_returns_sub(app_client: AsyncClient, dummy_access_token: str, cache_cognito_metadata_httpx_mock):
     response = await app_client.get(
         '/me',
         headers={'Authorization': f'Bearer {dummy_access_token}'}
