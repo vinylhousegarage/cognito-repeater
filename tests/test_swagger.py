d7c71521f2ef47f8ce49e40e59f3df8457965b3b
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-05 06:12:34 +0900

    test(swagger): add test_redoc_with_valid_token

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -13,3 +13,11 @@ async def test_docs_with_valid_token(app_client, test_access_token, cache_cognit
 async def test_redoc_is_disabled(app_client):
     response = await app_client.get('/redoc')
     assert response.status_code == 403
+
+async def test_redoc_with_valid_token(app_client, test_access_token, cache_cognito_metadata_httpx_mock):
+    response = await app_client.get(
+        '/redoc',
+        headers={'Authorization': f'Bearer {test_access_token}'}
+    )
+    assert response.status_code == 200
+    assert 'ReDoc' in response.text
