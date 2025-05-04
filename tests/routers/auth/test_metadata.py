e01e8ee3e75be5b50e1baf2ea58ed1be1b9ae1bb
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-05 07:57:02 +0900

    test(metadata) add test_metadata_response_valid

---
+++ tests/routers/auth/test_metadata.py
@@ -0,0 +1,15 @@
+from app.routers.api_docs import MetadataResponse
+
+def test_metadata_response_valid():
+    response = MetadataResponse(
+        login_endpoint='/login',
+        logout_endpoint='/logout',
+        verify_access_token_endpoint='/me',
+        verify_userinfo_endpoint='/sub',
+        health_check_endpoint='/health',
+        simulate_404_endpoint='/error/404',
+        docs_url='/docs',
+        redoc_url='/redoc',
+        openapi_url='/openapi.json'
+    )
+    assert response.login_endpoint == '/login'
