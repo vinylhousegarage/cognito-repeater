0f6aaaa051411f89ef54661ff2533ddaa356c1e8
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-05 04:49:51 +0900

    fix(test): replace client with app_client

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -1,3 +1,3 @@
-def test_docs_is_disabled(client):
-    response = client.get('/docs')
+def test_docs_is_disabled(app_client):
+    response = app_client.get('/docs')
     assert response.status_code == 404
