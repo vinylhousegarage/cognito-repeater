0ba2166f86945497873f6f8760dedc5463ce8cf1
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-05 06:25:10 +0900

    feat(swagger): add protected_openapi

--- app/routers/api_docs.py
+++ app/routers/api_docs.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, Request
 from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
+from fastapi.responses import JSONResponse
+from fastapi.openapi.utils import get_openapi
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.utils.jwt_helpers import verify_access_token_only
 
@@ -17,3 +19,14 @@ async def protected_redoc(request: Request, token: HTTPAuthorizationCredentials 
     access_token = token.credentials
     await verify_access_token_only(request, access_token)
     return get_redoc_html(openapi_url='/openapi.json', title='Protected ReDoc')
+
+@router.get('/openapi.json')
+async def protected_openapi(request: Request, token: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
+    access_token = token.credentials
+    await verify_access_token_only(request, access_token)
+    app = request.app
+    return JSONResponse(get_openapi(
+        title=app.title,
+        version=app.version,
+        routes=app.routes,
+    ))
