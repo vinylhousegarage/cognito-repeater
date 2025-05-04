6508e2cf72bb485006cf0d0866d0538ece91c99b
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-05 05:10:23 +0900

    feat(test): add test_docs_with_valid_token

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -1,3 +1,11 @@
 async def test_docs_is_disabled(app_client):
     response = await app_client.get('/docs')
     assert response.status_code == 403
+
+async def test_docs_with_valid_token(app_client, dummy_access_token):
+    response = await app_client.get(
+        '/docs',
+        headers={'Authorization': f'Bearer {dummy_access_token}'}
+    )
+    assert response.status_code == 200
+    assert 'Swagger UI' in response.text
