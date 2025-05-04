a207e414e1b4b5da6bdfd1aed1740ffb889d3032
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-05 05:23:33 +0900

    fix(test): mock app.state in test_docs_with_valid_token

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -2,7 +2,11 @@ async def test_docs_is_disabled(app_client):
     response = await app_client.get('/docs')
     assert response.status_code == 403
 
-async def test_docs_with_valid_token(app_client, dummy_access_token):
+async def test_docs_with_valid_token(app, app_client, dummy_access_token):
+    metadata_url = 'https://example.com/metadata_url'
+    app.state.config = type('Config', (), {})()
+    app.state.config.AWS_COGNITO_METADATA_URL = metadata_url
+
     response = await app_client.get(
         '/docs',
         headers={'Authorization': f'Bearer {dummy_access_token}'}
