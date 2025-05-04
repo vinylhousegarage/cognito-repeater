01988aaf8da3a97b8a6fd5b574d44fd041ade75a
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-05 04:57:41 +0900

    fix(test): replace def with async/await; change assertion status_code to 401

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -1,3 +1,3 @@
-def test_docs_is_disabled(app_client):
-    response = app_client.get('/docs')
-    assert response.status_code == 404
+async def test_docs_is_disabled(app_client):
+    response = await app_client.get('/docs')
+    assert response.status_code == 401
