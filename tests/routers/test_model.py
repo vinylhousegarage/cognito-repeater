d56372f65154a6053358b3e8444367b241d038e3
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-05 14:44:35 +0900

    test(model): add test_metadata_response_invalid_path

--- tests/routers/test_model.py
+++ tests/routers/test_model.py
@@ -1,3 +1,5 @@
+import pytest
+from pydantic import ValidationError
 from app.model import MetadataResponse
 
 def test_metadata_response_valid():
@@ -13,3 +15,19 @@ def test_metadata_response_valid():
         openapi_url='/openapi.json'
     )
     assert response.login_endpoint == '/login'
+
+def test_metadata_response_invalid_path():
+    with pytest.raises(ValidationError) as exc:
+        MetadataResponse(
+            login_endpoint='login',
+            logout_endpoint='/logout',
+            verify_access_token_endpoint='/me',
+            verify_userinfo_endpoint='/sub',
+            health_check_endpoint='/health',
+            simulate_404_endpoint='/error/404',
+            docs_url='/docs',
+            redoc_url='/redoc',
+            openapi_url='/openapi.json'
+        )
+    assert "must start with '/'" in str(exc.value)
+
