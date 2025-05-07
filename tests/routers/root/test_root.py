af5a7a0a561b7321c5798d1b48e38f3eb77322f2
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-08 05:00:25 +0900

    fix(test): rewrite test_root_endpoint with async app_client

--- tests/routers/root/test_root.py
+++ tests/routers/root/test_root.py
@@ -1,10 +1,4 @@
-from fastapi.testclient import TestClient
-from app import create_app
-
-app = create_app()
-client = TestClient(app)
-
-def test_root_endpoint_redirects_to_login():
-    response = client.get('/', allow_redirects=False)
+async def test_root_endpoint_redirects_to_login(app_client):
+    response = app_client.get('/', allow_redirects=False)
     assert response.status_code == 307
     assert response.headers['location'] == '/login'
