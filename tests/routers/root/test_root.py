f293eb86811b7e366a5bd28108baddff72cf13fd
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-08 05:14:10 +0900

    fix(test): add missing await and remove allow_redirects argument

--- tests/routers/root/test_root.py
+++ tests/routers/root/test_root.py
@@ -1,4 +1,4 @@
 async def test_root_endpoint_redirects_to_login(app_client):
-    response = app_client.get('/', allow_redirects=False)
+    response = await app_client.get('/')
     assert response.status_code == 307
     assert response.headers['location'] == '/login'
