98f29da73f99c4c9c30cf59a6487b74f2c8b4658
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-08 05:22:35 +0900

    chore(test): rename test_{health,errors}_status_code to *_endpoint_status_code

--- tests/routers/test_errors.py
+++ tests/routers/test_errors.py
@@ -8,6 +8,6 @@ client = TestClient(app)
 @pytest.mark.parametrize('path,expected_status', [
     ('/error/404', 404),
 ])
-def test_errors_status_code(path, expected_status):
+def test_errors_endpoint_status_code(path, expected_status):
     response = client.get(path, follow_redirects=False)
     assert response.status_code == expected_status
