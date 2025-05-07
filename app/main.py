8bd38cf8ca620ef8c659d2c1c7a2d35d5d87923b
vinylhousegarage <vinylhousegarage@gmail.com>
2025-05-08 06:16:21 +0900

    refactor(logger): log python version using sys.version

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 import subprocess
 from mangum import Mangum
@@ -8,6 +9,7 @@ logger.setLevel(logging.INFO)
 
 def log_package_versions():
     logger.info('log_package_versions() was called!')
+    logger.info('Python version: %s', sys.version)
     try:
         result = subprocess.run(['pip', 'freeze'], capture_output=True, text=True, check=True)
         logger.info('Installed packages:\n%s', result.stdout)
